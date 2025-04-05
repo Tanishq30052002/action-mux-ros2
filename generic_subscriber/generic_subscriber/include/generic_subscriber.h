07dd779c44c03efc4ca5391ee8f87e000a38f1b2
Tanishq Chaudhary <tanishqchaudhary101010@gmail.com>
2025-04-05 18:50:12 +0530

    removed deserialization

--- generic_subscriber/generic_subscriber/include/generic_subscriber.h
+++ generic_subscriber/generic_subscriber/include/generic_subscriber.h
@@ -6,14 +6,9 @@
 #include <string>
 #include <yaml-cpp/yaml.h>
 
-#include "dlfcn.h"
 #include "dynmsg/message_reading.hpp"
+#include "dynmsg/typesupport.hpp"
 #include "dynmsg/yaml_utils.hpp"
-
-#include "rosidl_typesupport_fastrtps_c/identifier.h"
-#include "rosidl_typesupport_fastrtps_cpp/identifier.hpp"
-#include "rosidl_typesupport_fastrtps_cpp/message_type_support.h"
-#include "rosidl_typesupport_fastrtps_cpp/service_type_support.h"
 
 class GenericSubscriber : public rclcpp::Node {
 public:
--- generic_subscriber/generic_subscriber/src/generic_subscriber.cpp
+++ generic_subscriber/generic_subscriber/src/generic_subscriber.cpp
@@ -60,7 +60,7 @@ void GenericSubscriber::create_subscription() {
 }
 
 void GenericSubscriber::getTopicTypeFromString(
-    std::pair<std::string, std::string> &topic_type_pair) {
+    InterfaceTypeName &topic_type_pair) {
   std::string tmp = detected_type_;
   size_t pos = tmp.find("/msg");
   if (pos != std::string::npos) {
@@ -75,89 +75,21 @@ void GenericSubscriber::getTopicTypeFromString(
   topic_type_pair.second = tmp.substr(split_at + 1);
 }
 
-typedef const rosidl_message_type_support_t *(*get_message_ts_func)();
-using TypeInfo_Cpp = rosidl_typesupport_introspection_cpp::MessageMembers;
-
 void GenericSubscriber::generic_subscriber_callback(
     std::shared_ptr<rclcpp::SerializedMessage> msg) {
-
-  std::pair<std::string, std::string> interface_type;
+  InterfaceTypeName interface_type;
   getTopicTypeFromString(interface_type);
-  const std::string pkg_name = interface_type.first;
-  const std::string msg_name = interface_type.second;
 
-  std::string ts_lib_name =
-      "lib" + pkg_name + "__rosidl_typesupport_introspection_cpp.so";
-
-  RCUTILS_LOG_DEBUG_NAMED("dynmsg",
-                          "Loading C++ introspection type support library %s",
-                          ts_lib_name.c_str());
-  void *introspection_type_support_lib = dlopen(ts_lib_name.c_str(), RTLD_LAZY);
-  if (nullptr == introspection_type_support_lib) {
-    RCUTILS_LOG_ERROR_NAMED(
-        "dynmsg", "failed to load C++ introspection type support library: %s",
-        dlerror());
-  }
-
-  std::string ts_func_name = "_ZN36rosidl_typesupport_introspection_cpp31get_"
-                             "message_type_support_handleIN" +
-                             std::to_string(pkg_name.length()) + pkg_name +
-                             "3msg" + std::to_string(msg_name.length() + 1) +
-                             msg_name +
-                             "_ISaIvEEEEEPK29rosidl_message_type_support_tv";
-  RCUTILS_LOG_DEBUG_NAMED("dynmsg", "Loading C++ type support function %s",
-                          ts_func_name.c_str());
-
-  get_message_ts_func introspection_type_support_handle_func =
-      reinterpret_cast<get_message_ts_func>(
-          dlsym(introspection_type_support_lib, ts_func_name.c_str()));
-
-  if (nullptr == introspection_type_support_handle_func) {
-    RCUTILS_LOG_ERROR_NAMED(
-        "dynmsg", "failed to load C++ introspection type support function: %s",
-        dlerror());
-  }
-
-  // Call the function to get the introspection information we want
-  const rosidl_message_type_support_t *introspection_ts =
-      introspection_type_support_handle_func();
-  RCUTILS_LOG_DEBUG_NAMED("dynmsg", "Loaded C++ type support %s",
-                          introspection_ts->typesupport_identifier);
-
-  const TypeInfo_Cpp *type_info =
-      reinterpret_cast<const TypeInfo_Cpp *>(introspection_ts->data);
-
+  RosMessage_Cpp ros_msg;
+  ros_msg.type_info = dynmsg::cpp::get_type_info(interface_type);
   rcl_allocator_t *msg_alloc =
       &msg.get()->get_rcl_serialized_message().allocator;
-  uint8_t *data = static_cast<uint8_t *>(
-      msg_alloc->allocate(type_info->size_of_, msg_alloc->state));
-  type_info->init_function(data,
-                           rosidl_runtime_cpp::MessageInitialization::ZERO);
-  RosMessage_Cpp ros_msg;
-  rclcpp::SerializationBase serializer(introspection_ts);
-  auto ptr = msg.get();
-  RCLCPP_INFO(this->get_logger(), "%p\n ", (void *)ptr);
-  void *des_msg = operator new(type_info->size_of_);
 
-  if ((void *)introspection_ts->data == nullptr)
-    RCLCPP_INFO(this->get_logger(), "data is null");
-  if ((void *)introspection_ts->func == nullptr)
-    RCLCPP_INFO(this->get_logger(), "func is null");
-  if ((void *)introspection_ts->typesupport_identifier == nullptr)
-    RCLCPP_INFO(this->get_logger(), "type_identifier is null");
+  ros_msg.data = static_cast<uint8_t *>(
+      msg_alloc->allocate(ros_msg.type_info->size_of_, msg_alloc->state));
 
-  auto t = introspection_ts->func(
-      introspection_ts,
-      rosidl_typesupport_fastrtps_cpp::typesupport_identifier);
-  if (t == nullptr)
-    RCLCPP_INFO(this->get_logger(), "t is null");
-
-  RCLCPP_INFO(this->get_logger(), "before deserializer");
-  serializer.deserialize_message(ptr, des_msg);
-  RCLCPP_INFO(this->get_logger(), "after deserializer");
-
-  ros_msg.data = data;
-  ros_msg.type_info = type_info;
+  ros_msg.type_info->init_function(
+      ros_msg.data, rosidl_runtime_cpp::MessageInitialization::ZERO);
 
   auto yaml_msg = dynmsg::cpp::message_to_yaml(ros_msg);
   auto string_msg = dynmsg::yaml_to_string(yaml_msg, true, false);
@@ -165,4 +97,7 @@ void GenericSubscriber::generic_subscriber_callback(
               "[generic_subscriber_callback]\nTopic: %s\nDetected Type: "
               "%s\nROS2 Message:\n%s",
               topic_name_.c_str(), detected_type_.c_str(), string_msg.c_str());
+
+  ros_msg.type_info->fini_function(ros_msg.data);
+  msg_alloc->deallocate(ros_msg.data, msg_alloc->state);
 }
